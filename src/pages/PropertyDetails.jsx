79ea4d7a10b564d9c5bad6e073ee5f0707bb12dc
ronney omondi <ronneyomondi14@gmail.com>
2026-06-14 21:05:08 +0300

    added PropertyDetails.jsx

--- src/pages/PropertyDetails.jsx
+++ src/pages/PropertyDetails.jsx
@@ -1,7 +1,37 @@
+import { useParams} from "reactrouter-dom";
+import { useState, useEffect } from "react";
+
 export default function PropertyDetails() {
+   const { id } = useParams();
+  const [property, setProperty] = useState(null);
+
+  useEffect(() => {
+    fetch(`/api/properties/${id}`)
+      .then(response => response.json())
+      .then(data => setProperty(data))
+      .catch(error => console.error("Error fetching property details:", error));
+  }, [id]);
+
+  if (!property) {
+    return <h2>loading...</h2>;
+  }
   return (
     <div>
       <h1>Property Details</h1>
+      <img src={property.image_url} alt={property.name} />
+
+      <h3>location</h3>
+      <p>{property.location}</p>
+
+      <h3>Rent</h3>
+      <p>KES {property.rent}</p>
+
+      <h3>Amenities</h3>
+      <ul>
+        {property.amenities.map((amenity, index) => (
+          <li key={index}>{amenity}</li>
+        ))}
+      </ul>
     </div>
   );
 }
