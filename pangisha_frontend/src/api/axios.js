3bc8feacea2dac031f2a3352032ea79a80da3206
ronney omondi <ronneyomondi14@gmail.com>
2026-07-03 17:10:22 +0300

    Update frontend API URL

--- pangisha_frontend/src/api/axios.js
+++ pangisha_frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5555",
+  baseURL: "https://pangisha-smarthouse-c7ek.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
