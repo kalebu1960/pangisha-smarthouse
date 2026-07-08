98a27e4c644d09aaa6cded73556b2b29990e5719
ronney omondi <ronneyomondi14@gmail.com>
2026-07-08 20:39:14 +0300

    changed config.py

--- server/config.py
+++ server/config.py
@@ -4,7 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/mydatabase"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+    "DATABASE_URL",
+    "postgresql://localhost/mydatabase"
+)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
-    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", SECRET_KEY)
